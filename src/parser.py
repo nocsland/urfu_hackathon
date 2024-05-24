d7922a9873b0525afb862f77fc3d696105a8f335
Anton Vlasov <nocsland@gmail.com>
2024-05-25 03:07:26 +0400

    Add parser and requirements.txt

---
+++ src/parser.py
@@ -0,0 +1,32 @@
+
+import pickle
+import warnings
+import re
+
+import spacy
+from joblib import Memory
+from langchain_community.document_loaders import DirectoryLoader
+
+# Отключаем все предупреждения
+warnings.filterwarnings("ignore")
+
+# Загружаем исходные данные
+loader = DirectoryLoader("../data/source_text/test")
+docs = loader.load()
+
+# Очищаем исходный текст
+nlp = spacy.load('ru_core_news_sm')
+for item in docs:
+    doc = nlp(item.page_content)
+    tokens = [
+        str(token)  # .lemma_
+        for token in doc
+        if not token.is_stop and not token.is_punct and not token.is_space]
+    cleaned_text = ' '.join(tokens).strip()
+    cleaned_text = re.sub(r'меток Обзор|Постоянная ссылка|Инструменты контента.*$', '', cleaned_text)
+    cleaned_text = re.sub(r'^.*Переход началу метаданных', '', cleaned_text)
+    item.page_content = cleaned_text
+
+# Запись списка объектов в файл
+with open('../data/out/pkl/documents.pkl', 'wb') as file:
+    pickle.dump(docs, file)
